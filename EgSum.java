60d6cae31e128f094ca5fb81d5ec3b6b3205e8a8
Vyshnavi-14 <91303086+Vyshnavi-14@users.noreply.github.com>
2021-12-02 10:41:48 +0530

    Add files via upload

---
+++ EgSum.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class EgSum {
+    public static void main(String args[])
+    {
+        int a=8,b=9;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    
+}
